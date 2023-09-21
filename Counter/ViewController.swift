f7ffa9e6bc0639606593df492af80a6436f6f6d6
Mikhail Frantsuzov <m.francuzov@gmail.com>
2023-09-21 12:38:10 +0200

    - Добавлены модификаторы доступа "private" для переменных и функций - Поправлены отступы - Включена проверка правописания

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,14 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var counterValue = 0
-    @IBOutlet weak var labelCount: UILabel!
-    @IBOutlet weak var buttonPush: UIButton!
+    private var counterValue = 0
+    @IBOutlet weak private var labelCount: UILabel!
+    @IBOutlet weak private var buttonPush: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         labelCount.text = "Значение счётчика: \(counterValue)"
     }
-    @IBAction func buttonDidTap(_ sender: Any) {
+    @IBAction private func buttonDidTap(_ sender: Any) {
         counterValue += 1
         labelCount.text = "Значение счётчика: \(counterValue)"
     }
